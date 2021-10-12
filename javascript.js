debd215a9c97e068371a6d81e3d3997f500270a9
Mokappa <75574992+Mokappa@users.noreply.github.com>
2021-10-12 12:23:58 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,34 @@
+//Loading Animation
+setTimeout( function() {
+    $('.preloader').fadeToggle();
+}, 4000);
+
+//Parallax text
+let text = document.getElementById("title");
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    text.style.paddingTop = value * 0.5 + 'px';
+});
+
+
+/*GET LENGTH OF PATHS
+const logo = document.querySelectorAll("#logo path");
+
+for( let i = 0; i < logo.length; ++ i )
+{
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}*/
+
+
+/*NAV COLOR AFTER SCROLLING
+var nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function() {
+    if( window.pageYOffset > 100 ) {
+        nav.classList.add('bg-dark', 'shadow');
+    } else {
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+});
+*/
